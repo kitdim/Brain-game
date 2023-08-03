2e76b635a0f0db450bfbf6cc9f4e4e44baa8ce94
kitdim <kitaykin9808@gmail.com>
2023-08-03 17:58:51 +0300

    add task№2

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,6 @@
 package hexlet.code;
 public class App {
     public static void main(String[] args) {
-        System.out.printf("Hello World!");
+        System.out.println("Welcome to the Brain Games!");
     }
 }
