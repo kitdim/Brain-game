5114dce921b8b43c6c53dd0400908f4b1040aa93
kitdim <kitaykin9808@gmail.com>
2023-08-05 21:26:28 +0300

    add Nod.java

---
+++ app/src/main/java/hexlet/code/game/Nod.java
@@ -0,0 +1,52 @@
+package hexlet.code.game;
+
+import java.util.Random;
+
+public class Nod {
+    private final String title;
+    private int operand1;
+    private int operand2;
+    private int nod;
+
+    public Nod() {
+        title = "What number is gcd of two numbers?";
+        nod = 0;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getOperand1() {
+        return operand1;
+    }
+
+    public int getOperand2() {
+        return operand2;
+    }
+
+    public String getNod() {
+
+        if (operand1 < operand2) {
+            operand1 += operand2;
+            operand2 = operand1 - operand2;
+            operand1 -= operand2;
+        }
+
+        for (int i = 1; i <= operand2; i++) {
+            if (operand1 % i == 0 && operand2 % i == 0) {
+                nod = i;
+            }
+        }
+
+        return Integer.toString(nod);
+    }
+
+    public void setOperand1(byte round) {
+        operand1 = new Random().nextInt(round) + 1;
+    }
+
+    public void setOperand2(byte round) {
+        operand2 = new Random().nextInt(round) + 1;
+    }
+}
