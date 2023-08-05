62590abb8bf14f1ca7603ed5efa78c463de78930
kitdim <kitaykin9808@gmail.com>
2023-08-05 20:31:33 +0300

    update Even

---
+++ app/src/main/java/hexlet/code/game/Even.java
@@ -0,0 +1,26 @@
+package hexlet.code.game;
+
+import java.util.Random;
+
+public class Even{
+    private final String title;
+    private int number;
+    public Even() {
+        title = "Answer 'yes' if number is even otherwise answer 'no'.";
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(byte roundRandomNumbers) {
+        this.number = new Random().nextInt(roundRandomNumbers) + 1;
+    }
+
+    public boolean isEven() {
+        return number % 2 == 0;
+    }
+}
