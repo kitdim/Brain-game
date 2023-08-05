bffcce72bc5e63dcfaea744ab21d7ed422a26417
kitdim <kitaykin9808@gmail.com>
2023-08-05 20:30:32 +0300

    add in menu a task 4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,28 +6,16 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
         Scanner input = new Scanner(System.in);
-        byte change;
+        byte numberOfGame;
         System.out.println("""
                 Please enter the game number and press Enter.
                 1 - Greet
                 2 - Even
+                3 - Calc
                 0 - Exit""");
         System.out.print("Your change: ");
-        change = input.nextByte();
+        numberOfGame = input.nextByte();
         System.out.println();
-        switch (change) {
-            case 0:
-                break;
-            case 1:
-                System.out.print("May i have your name? ");
-                String name = input.next();
-                System.out.println("Hello, " + name + "!");
-                break;
-            case 2:
-                Even.runEven();
-                break;
-            default:
-                break;
-        }
+        Engine.game(numberOfGame);
     }
 }
