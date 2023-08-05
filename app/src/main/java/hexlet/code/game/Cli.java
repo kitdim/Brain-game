b5839dcc45c169eff172f8f14befff1e3e3d0a41
kitdim <kitaykin9808@gmail.com>
2023-08-05 20:31:20 +0300

    update Cli

--- app/src/main/java/hexlet/code/game/Cli.java
+++ app/src/main/java/hexlet/code/game/Cli.java
@@ -1,11 +1,12 @@
-package hexlet.code;
+package hexlet.code.game;
 import java.util.Scanner;
 
 public class Cli {
-    public static void hello() {
+    public static String getUserName() {
         Scanner input = new Scanner(System.in);
         System.out.print("May i have your name? ");
         String name = input.next();
-        System.out.println("Hello " + name + "!");
+        System.out.printf("Hello " + name + "!");
+        return name;
     }
 }
