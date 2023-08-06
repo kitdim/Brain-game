020fdbd669a451fdfb6e1888890181647f62bec0
kitdim <kitaykin9808@gmail.com>
2023-08-06 13:04:43 +0300

    update Calc.java

--- app/src/main/java/hexlet/code/game/Calc.java
+++ app/src/main/java/hexlet/code/game/Calc.java
@@ -3,7 +3,7 @@ package hexlet.code.game;
 import java.util.Random;
 
 public class Calc {
-    private final String title;
+    private final String TITLE;
     private final char[] operations;
     private char operation;
     private int operand1;
@@ -11,12 +11,12 @@ public class Calc {
     private int result;
     public Calc() {
         operations = new char[]{'+', '-', '*'};
-        title = "What is the result of the expression?";
+        TITLE = "What is the result of the expression?";
         result = 0;
     }
 
     public String getTitle() {
-        return title;
+        return TITLE;
     }
     public int getOperand1() {
         return operand1;
