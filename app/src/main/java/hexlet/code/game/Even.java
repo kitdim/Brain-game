0d29458e6ba7e49fa251d4698787c30beda4e9b7
kitdim <kitaykin9808@gmail.com>
2023-08-11 13:24:04 +0300

    update Even

--- app/src/main/java/hexlet/code/game/Even.java
+++ app/src/main/java/hexlet/code/game/Even.java
@@ -5,17 +5,17 @@ import hexlet.code.Utils;
 import hexlet.code.Engine;
 
 public class Even {
-    private static int operand;
-
     public static void start() {
         String userName = Cli.getUserName();
         Cli.hello(userName);
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
         for (int i = Engine.COUNT_OF_ATTEMPTS; i > 0; i--) {
-            operand = Utils.getRandomNumber();
-            String result = isEven() ? "yes" : "no";
-            String answer = Engine.buildGame(operand, result);
-            if (answer.equals("yes")) {
+            int operand = Utils.getRandomNumber();
+            String question = Integer.toString(operand);
+            String result = isEven(operand) ? "yes" : "no";
+            Engine.setQuestion(question);
+            System.out.println(Engine.getQuestion());
+            if (Engine.checkAnswer(result)) {
                 Engine.showAnswer();
             } else {
                 Engine.showAnswer(result);
@@ -25,7 +25,7 @@ public class Even {
         Engine.showResult(userName);
     }
 
-    public static boolean isEven() {
+    public static boolean isEven(int operand) {
         return operand % 2 == 0;
     }
 }
