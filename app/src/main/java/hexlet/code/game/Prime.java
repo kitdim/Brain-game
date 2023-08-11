d18e239690683ac9ff7439d15e0bbe1e4079fec4
kitdim <kitaykin9808@gmail.com>
2023-08-11 16:19:46 +0300

    fix https://github.com/kitdim/java-project-61/blob/main/app/src/main/java/hexlet/code/game/Prime.java#L28-L38  данный метод лучше сделать независящим от полей класса. Универсальный метод будет принимать на вход два значения и оператор.

--- app/src/main/java/hexlet/code/game/Prime.java
+++ app/src/main/java/hexlet/code/game/Prime.java
@@ -5,17 +5,18 @@ import hexlet.code.Engine;
 import hexlet.code.Utils;
 
 public final class Prime {
-    private static int number;
 
     public static void start() {
         String userName = Cli.getUserName();
         Cli.hello(userName);
         System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
         for (int i = Engine.COUNT_OF_ATTEMPTS; i > 0; i--) {
-            number = Utils.getRandomNumber();
-            String result = isPrime() ? "yes" : "no";
-            String answer = Engine.buildGame(number, result);
-            if (answer.equals("yes")) {
+            int number = Utils.getRandomNumber();
+            String question = Integer.toString(number);
+            String result = isPrime(number) ? "yes" : "no";
+            Engine.setQuestion(question);
+            System.out.println(Engine.getQuestion());
+            if (Engine.checkAnswer(result)) {
                 Engine.showAnswer();
             } else {
                 Engine.showAnswer(result);
@@ -25,7 +26,7 @@ public final class Prime {
         Engine.showResult(userName);
     }
 
-    private static boolean isPrime() {
+    private static boolean isPrime(int number) {
         if (number == 0) {
             return  false;
         }
