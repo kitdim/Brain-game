f6515928b5b44b849bc7d05e347b98d82f6b687b
kitdim <kitaykin9808@gmail.com>
2023-08-16 20:27:42 +0300

    update Utils.java

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -3,9 +3,7 @@ package hexlet.code;
 import java.util.Random;
 
 public class Utils {
-    private static final Random RNB = new Random();
-
     public static int getRandomNumber(int min, int max) {
-        return RNB.nextInt(min, max);
+        return new Random().nextInt(min, max);
     }
 }
